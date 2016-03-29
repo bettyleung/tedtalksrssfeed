9a8429936e6586f9eee37b52f2fcb0ec3936a36b
Betty <leung@ucdavis.edu>
2016-03-29 16:08:24 -0700

    cleaned up code & added comments

--- js/feed.js
+++ js/feed.js
@@ -1,3 +1,4 @@
+//triggers google feeds
 function initialize() {
   var feed = new google.feeds.Feed("http://feeds.feedburner.com/tedtalks_video");
   feed.load(function(result) {
@@ -7,6 +8,7 @@ function initialize() {
   });
 }
 
+//creates the table to display the feed data
 function setFeedData(result){
   var containerTable = document.getElementById("feedTableBody");
 
@@ -16,7 +18,7 @@ function setFeedData(result){
     var tdTagTitle = document.createElement("td");
     var tdTagAuthor = document.createElement("td");
     var tdTagCatgegory = document.createElement("td");
-    var tdTagButton= document.createElement("td");
+    var tdTagButton = document.createElement("td");
     var expandIcon= document.createElement("i");
     var expandBtn = document.createElement("button");
     var centerTag = document.createElement("center");
@@ -31,15 +33,13 @@ function setFeedData(result){
     tdTagCatgegory.appendChild(document.createTextNode(entry.publishedDate.substr(0, entry.publishedDate.length-15)));
     centerTag.appendChild(expandBtn)
     tdTagButton.appendChild(centerTag);
-
-
     trTag.appendChild(tdTagTitle);
     trTag.appendChild(tdTagAuthor);
     trTag.appendChild(tdTagCatgegory);
     trTag.appendChild(tdTagButton);
     containerTable.appendChild(trTag);
 
-    setModal(i, entry);
+    setModalData(i, entry);
     expandBtn.onclick = function(){
       showModal(this);
     };
@@ -47,67 +47,72 @@ function setFeedData(result){
   }
 }
 
-
-function setModal(btnNum, entry){
+//create modal that will display feed metadata
+function setModalData(btnNum, entry){
   var build = document.getElementById('buildModal');
   var divModal = document.createElement("div");
   var divModalContent = document.createElement("div");
   var divModalHeader = document.createElement("div");
-  var divModalBody = document.createElement("div");
   var spanModal = document.createElement("span");
   var newlineTag = document.createElement("br");
-  var centerTag = document.createElement("center");
 
   $(divModal).attr("id","modalFeed"+btnNum);
   $(divModal).addClass("modal");
   $(divModalContent).addClass("modal-content");
   $(divModalHeader).addClass("modal-header");
-  $(divModalBody).addClass("modal-body");
   $(spanModal).addClass("close");
   $(spanModal).attr("id","spanModal"+btnNum);
-
   $(spanModal).text("x");
 
-  //image
-  var imgTag = document.createElement("img");
-  var divImg = document.createElement("div");
-  $(imgTag).attr("src", entry.mediaGroups[0].contents[0].thumbnails[0].url);
-  $(divImg).addClass("resizeimg");
-  divImg.appendChild(imgTag);
-  centerTag.appendChild(divImg);
-
-  centerTag.appendChild(newlineTag);
-  divModalBody.appendChild(centerTag);
-  var paragraph = document.createElement("p");
-  var paragraph2 = document.createElement("p");
-  var paragraph3 = document.createElement("p");
-  $(paragraph).addClass("title");
-  paragraph.appendChild(document.createTextNode(entry.title))
-  divModalBody.appendChild(paragraph);
-  paragraph2.appendChild(document.createTextNode(entry.content.substr(0, entry.content.indexOf('<img src'))));
-  divModalBody.appendChild(paragraph2);
-
-  var centerFooterTag = document.createElement("center");
-  $(paragraph3).append(document.createTextNode("See full post "))
-  $(paragraph3).append("<a href=" + entry.link + ">here.</a>");
-  centerFooterTag.appendChild(paragraph3)
-  divModalBody.appendChild(centerFooterTag);
-
-
+  var imgTag = buildModalImage(entry, newlineTag)
+  var divModalBody = buildModalBody(imgTag, entry, newlineTag);
 
   divModalHeader.appendChild(spanModal);
   divModalContent.appendChild(divModalHeader);
   divModalContent.appendChild(divModalBody);
   divModal.appendChild(divModalContent);
-
   build.appendChild(divModal);
-
-
-
 }
 
+//help function that creates the image on the modal
+function buildModalImage(entry, newlineTag){
+  var imgTag = document.createElement("img");
+  var divImg = document.createElement("div");
+  var centerTag = document.createElement("center");
+
+  $(imgTag).attr("src", entry.mediaGroups[0].contents[0].thumbnails[0].url);
+  $(divImg).addClass("resizeimg");
+  divImg.appendChild(imgTag);
+  centerTag.appendChild(divImg);
+  centerTag.appendChild(newlineTag);
+  return centerTag;
+}
+
+//help function that creates the text on the modal
+function buildModalBody(imgTag, entry, newlineTag){
+  var divModalBody = document.createElement("div");
+  var paragraphTitle = document.createElement("p");
+  var paragraphContent = document.createElement("p");
+  var paragraphLink = document.createElement("p");
+  var centerFooterTag = document.createElement("center");
+
+  $(divModalBody).addClass("modal-body");
+  $(paragraphTitle).addClass("title");
+  $(paragraphLink).append(document.createTextNode("See full post "))
+  $(paragraphLink).append("<a href=" + entry.link + ">here.</a>");
+  paragraphTitle.appendChild(document.createTextNode(entry.title));
+  paragraphContent.appendChild(document.createTextNode(entry.content.substr(0, entry.content.indexOf('<img src'))));
+  centerFooterTag.appendChild(paragraphLink)
+  divModalBody.appendChild(imgTag);
+  divModalBody.appendChild(paragraphTitle);
+  divModalBody.appendChild(paragraphContent);
+  divModalBody.appendChild(centerFooterTag);
+
+  return divModalBody;
+}
+
+//triggers popup of modal on click of the binocular button
 function showModal(me){
-  //alert("im " + $(me).attr("id"));
 
   var btnNum = $(me).attr("id").slice(-1);
   var modal = document.getElementById("modalFeed" + btnNum);
